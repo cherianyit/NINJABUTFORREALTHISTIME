7c893038d759f0886094e4feb2654654365d9ad7
zhihil <32404412+zhihil@users.noreply.github.com>
2017-09-30 16:07:59 -0400

    Adding double-dash and double-jump
    See changes in playerScript

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -16,9 +16,20 @@ public class PlayerScript : MonoBehaviour {
 
 	Collider2D otherColl, stageColl;
 
-	bool isJumping = false, isHolding = true;
-
+	bool isJumping = false;
+	bool isHolding = true;
 	float dx, dy;
+
+	// double-jump variables
+	bool isDoubleJumping = false;
+	protected float doubleJump_cooldown = 0.5f;
+	protected int jump_count = 0;
+
+	// double-dash variables
+	protected float ButtonCooler = 0.5f;
+	protected int ButtonCount = 0;
+	int direction;
+	int magnitude;
 
 	// Use this for initialization
 	void Start () {
@@ -37,47 +48,70 @@ public class PlayerScript : MonoBehaviour {
 		Throw();
 	}
 
-	void MoveAndCollide() {
-		if (Input.GetKey(left)) {
-			this.transform.localScale = new Vector3(-6, 6, 1);
-			if (coll.IsTouching(otherColl) && otherNinja.transform.position.x < tf.position.x 
-				&& Mathf.Abs(otherNinja.transform.position.y - tf.position.y) < tf.localScale.y * coll.size.y) {
-				dx = 0;
-				if (!coll.IsTouching(stageColl)) {
-					isJumping = true;
-				}
-			} else {
-				dx = -speed;
-			}
-		}
-		if (Input.GetKey(right)) {
-			this.transform.localScale = new Vector3(6, 6, 1);
-			if (coll.IsTouching(otherColl) && otherNinja.transform.position.x > tf.position.x 
-				&& Mathf.Abs(otherNinja.transform.position.y - tf.position.y) < tf.localScale.y * coll.size.y) {
-				dx = 0;
-				if (!coll.IsTouching(stageColl)) {
-					isJumping = true;
-				}
-			} else {
-				dx = speed;
-			}
-		}
-		if (Input.GetKey(left) == Input.GetKey(right)) {
+	void Move(int xDirection) {
+		this.transform.localScale = new Vector3(Mathf.Sign(xDirection) * 6, 6, 1);
+		if (coll.IsTouching(otherColl) && otherNinja.transform.position.x > tf.position.x 
+			&& Mathf.Abs(otherNinja.transform.position.y - tf.position.y) < tf.localScale.y * coll.size.y) {
 			dx = 0;
+			if (!coll.IsTouching(stageColl)) {
+				isJumping = true;
+			}
+		} else {
+			dx = xDirection * speed;
 		}
-		if (Input.GetKey(up) && !isJumping) {
+	}
+
+	void MoveAndCollide() {
+		// determine direction and speed based on keys pressed
+		if (Input.GetKeyDown (left)) {
+			direction = -1;
+			magnitude = 1;
+			if (ButtonCooler > 0 && ButtonCount == 1 && !isJumping) {
+				magnitude = 3;
+			} else {
+				ButtonCooler = 0.5f;
+				ButtonCount += 1;
+			}
+		} else if (Input.GetKeyDown (right)) {
+			direction = 1;
+			magnitude = 1;
+			if (ButtonCooler > 0 && ButtonCount == 1 && !isJumping) {
+				magnitude = 3;
+			} else {
+				ButtonCooler = 0.5f;
+				ButtonCount += 1;
+			}
+		} else if (!Input.GetKey (left) && !Input.GetKey (right)) {
+			direction = 0;
+			magnitude = 0;
+		}
+
+		// calculate delay for double-dash
+		if (ButtonCooler > 0) {
+			ButtonCooler -= Time.deltaTime;
+		} else {
+			ButtonCount = 0;
+		}
+			
+		Move (magnitude * direction);
+
+		if (Input.GetKeyDown (up) && !isJumping) {
 			isJumping = true;
 			dy = jumpForce;
-		}
+		} else if (Input.GetKeyDown (up) && !isDoubleJumping) {
+			isDoubleJumping = true;
+			dy = 2f * jumpForce;
+		} 
 		tf.position += new Vector3(dx, 0, 0);
 	}
 
 	void Jump() {
-		if (isJumping) {
+		if (isJumping || isDoubleJumping) {
 			tf.position += new Vector3(0, dy, 0);
 			dy -= gravity;
 			if ((coll.IsTouching(stageColl) || coll.IsTouching(otherColl)) && dy < 0) {
 				isJumping = false;
+				isDoubleJumping = false;
 				dy = 0;
 			}
 		}
