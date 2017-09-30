0a0f659bcb171500300a0a9e4326957712db32d8
786213750 <786213750g@gmail.com>
2017-09-30 13:55:24 -0400

    star to star 2.0

---
+++ Assets/starScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starScript : MonoBehaviour {
+    bool isFlying = true;
+    bool hitplayer = false;
+    private Rigidbody2D rbody;
+    //private Collision2D coll;
+    public float Hr;
+    public float Vr;
+    public float velx;
+    public string eStar;
+    // Use this for initialization
+    void Start()
+    {
+
+        rbody = this.GetComponent<Rigidbody2D>();
+        //coll = GetComponent<Collision2D>();
+        rbody.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //OnCollisionEnter2D(coll);
+        if (isFlying)
+        {
+            
+            movement(velx, 0);
+        }
+        else
+        {
+            
+            movement(-velx, 0);
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == eStar)
+        {
+            Hr = rbody.velocity.x;
+            isFlying = false;
+            
+            //Vr = 1; 
+
+
+        }
+
+        if (coll.gameObject.name == "stage")
+        {
+            isFlying = false;
+            Hr = 0;
+            //Vr = 0;
+        }
+    }
+
+    void movement(float horizontal, float vertical)
+    {
+        rbody.velocity = new Vector2(horizontal, vertical);
+    }
+}
