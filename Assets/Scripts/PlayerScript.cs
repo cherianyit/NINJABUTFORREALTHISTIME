e6540b136715811268f06156259858f04b2e0b4f
zhihil <32404412+zhihil@users.noreply.github.com>
2017-09-30 18:39:39 -0400

    Added double-press dash

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,9 +10,9 @@ public class PlayerScript : MonoBehaviour {
 	public GameObject otherNinja, stage;
 	public StarScript star;
 
+	Rigidbody2D rb;
 	BoxCollider2D coll;
 	Transform tf;
-
 	Collider2D otherColl, stageColl;
 
 	bool isJumping = false, isKnockedBack = false, knockbackRight;
@@ -20,8 +20,18 @@ public class PlayerScript : MonoBehaviour {
 
 	float dx, dy;
 
+	// double press manager
+	protected float ButtonCooler = 0.5f;
+	protected int ButtonCount = 0;
+	int direction;
+	int magnitude;
+
 	// Use this for initialization
 	void Start () {
+		magnitude = 0;
+		direction = 0;
+
+		rb = this.GetComponent<Rigidbody2D>();
 		coll = this.GetComponent<BoxCollider2D>();
 		tf = this.transform;
 
@@ -44,40 +54,58 @@ public class PlayerScript : MonoBehaviour {
 		}
 	}
 
+
+	void Move(int xDirection) {
+		this.transform.localScale = new Vector3(Mathf.Sign(xDirection) * 6, 6, 1);
+		if (coll.IsTouching(otherColl) && otherNinja.transform.position.x > tf.position.x 
+			&& Mathf.Abs(otherNinja.transform.position.y - tf.position.y) < tf.localScale.y * coll.size.y) {
+			dx = 0;
+			if (!coll.IsTouching(stageColl)) {
+				isJumping = true;
+			}
+		} else {
+			dx = xDirection * speed;
+		}
+	}
+
 	void MoveAndCollide() {
 		if (!isKnockedBack) {
-			if (Input.GetKey(left)) {
-				this.transform.localScale = new Vector3(-6, 6, 1);
-				if (coll.IsTouching(otherColl) && otherNinja.transform.position.x < tf.position.x
-				   && Mathf.Abs(otherNinja.transform.position.y - tf.position.y) < tf.localScale.y * coll.size.y) {
-					dx = 0;
-					if (!coll.IsTouching(stageColl)) {
-						isJumping = true;
-					}
+			if (Input.GetKeyDown (left)) {
+				direction = -1;
+				magnitude = 1;
+				if (ButtonCooler > 0 && ButtonCount == 1) {
+					magnitude = 3;
 				} else {
-					dx = -speed;
+					ButtonCooler = 0.5f;
+					ButtonCount += 1;
 				}
-			}
-			if (Input.GetKey(right)) {
-				this.transform.localScale = new Vector3(6, 6, 1);
-				if (coll.IsTouching(otherColl) && otherNinja.transform.position.x > tf.position.x
-				   && Mathf.Abs(otherNinja.transform.position.y - tf.position.y) < tf.localScale.y * coll.size.y) {
-					dx = 0;
-					if (!coll.IsTouching(stageColl)) {
-						isJumping = true;
-					}
+			} else if (Input.GetKeyDown (right)) {
+				direction = 1;
+				magnitude = 1;
+				if (ButtonCooler > 0 && ButtonCount == 1) {
+					magnitude = 3;
 				} else {
-					dx = speed;
+					ButtonCooler = 0.5f;
+					ButtonCount += 1;
 				}
+			} else if (!Input.GetKey (left) && !Input.GetKey (right)) {
+				direction = 0;
+				magnitude = 0;
 			}
-			if (Input.GetKey(left) == Input.GetKey(right)) {
-				dx = 0;
+
+			if (ButtonCooler > 0) {
+				ButtonCooler -= Time.deltaTime;
+			} else {
+				ButtonCount = 0;
 			}
-			if (Input.GetKey(up) && !isJumping && (coll.IsTouching(stageColl)
-			   || (coll.IsTouching(otherColl)))) {
+
+			Move (magnitude * direction);
+
+			if (Input.GetKey(up) && !isJumping) {
 				isJumping = true;
 				dy = jumpForce;
 			}
+
 		}
 		tf.position += new Vector3(dx, 0, 0);
 	}
